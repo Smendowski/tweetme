075427149759ee96a08864945aaaf9d7fa7383ed
Mateusz Piotr <m.smendowski@gmail.com>
2020-06-22 20:10:01 +0200

    REST Api View

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 # importing Tweet from one dir above - best practice
 from .models import Tweet
@@ -10,8 +10,20 @@ def home_view(request, *args, **kwargs):
 # Dynamic url routing
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
   # We can retrieve a tweet only if it exsists in db. Otherwise error has occured with server
+  # RestAPI view - return json data
+  # consume by JS, Swift itd..
+  data = {
+    "id": tweet_id,
+    #"image_path": obj.image.url
+  }
+  status = 200
   try:
     obj = Tweet.objects.get(id=tweet_id)
+    data['content'] = obj.content
   except: 
-    raise Http404
-  return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} </h1>")
+    data['message'] = "Not found"
+    status = 404
+  
+  
+  # rest API view
+  return JsonResponse(data, status=status)
