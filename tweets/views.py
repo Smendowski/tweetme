7602f8b58373c81b87c69a30ac2ab6f9817e4445
Mateusz Piotr <m.smendowski@gmail.com>
2020-06-22 19:59:25 +0200

    Dynamic url handle
    - error with runserver:
    install django in workdir using pip

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+# importing Tweet from one dir above - best practice
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-  print(args, kwargs)
   return HttpResponse("<h1>HelloWorld</h1>")
 
 # Dynamic url routing
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-  print(args, kwargs)
-  return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+  # We can retrieve a tweet only if it exsists in db. Otherwise error has occured with server
+  try:
+    obj = Tweet.objects.get(id=tweet_id)
+  except: 
+    raise Http404
+  return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} </h1>")
