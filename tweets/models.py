99b9aa97651319c94832611241a0893d01cc2c8f
Mateusz Piotr <m.smendowski@gmail.com>
2020-06-24 19:50:47 +0200

    Reordering Tweets to see the oldest at the bottom
    - Modifying metadata in models requires migrations

--- tweets/models.py
+++ tweets/models.py
@@ -7,6 +7,10 @@ class Tweet(models.Model):
   content = models.TextField()
   image = models.FileField(upload_to='images/', blank=True, null=True)
 
+  # Reordering tweets to see the oldest at the bottom
+  class Meta:
+    ordering = ['-id']
+
   def serialize(self):
     return{
       "id": self.id,
