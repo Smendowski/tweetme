cec63fe487199b1a1a34e7799ae97222e16ab11e
Mateusz Piotr <m.smendowski@gmail.com>
2020-07-01 13:00:30 +0200

    Tweet Likes  - Django Admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+class TweetLikeAdmin(admin.TabularInline):
+  model = TweetLike
+
 
 class TweetAdmin(admin.ModelAdmin):
+  inlines = [TweetLikeAdmin]
   # każdy tweet niech bęzie wświetlony z id usera czyli user, zgodnie z metodą __str__ która jest w models.py. Sposób wyświetlania tweeta w django admin. domyslnie __str__ wyświetla cały obiekt Tweet. Jeśli nadpiszemy __str__ jako return self.content bedziemy w kolumanch mieli tylko content.
   list_display = ['__str__', 'user']
   # Django admin, dostaję search Boxa :)
